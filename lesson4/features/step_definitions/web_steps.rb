3af53d4942b4fd8cf021163ac719c13789e9f77c
fachrul choliluddin <fachrul.fch@gmail.com>
2019-07-24 22:07:44 +0700

    refoldering lesson4

--- lesson4/features/step_definitions/web_steps.rb
+++ lesson4/features/step_definitions/web_steps.rb
@@ -1,13 +1,13 @@
-When("I want to visit Bukalapak Homepage") do
-    @home = Homepage.new
-    @home.load
+When('I want to visit Bukalapak Homepage') do
+  @home = Homepage.new
+  @home.load
 end
 
-Then("I will see button Login") do
-    expect(@home).to have_login_button
+Then('I will see button Login') do
+  expect(@home).to have_login_button
 end
 
-Then("I will see Pencarian Populer section") do
-    @home.wait_for_pencarian_populer(10)
-    expect(@home).to have_pencarian_populer
+Then('I will see Pencarian Populer section') do
+  @home.wait_for_pencarian_populer(10)
+  expect(@home).to have_pencarian_populer
 end
